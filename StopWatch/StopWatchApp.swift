0515aa98ff48230d1ffe0d5339ddb3b8a2d7aeab
SuminJN <wjstnals1211@gmail.com>
2023-06-30 14:55:00 +0900

    Initial Commit

---
+++ StopWatch/StopWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopWatchApp.swift
+//  StopWatch
+//
+//  Created by 전수민 on 2023/06/30.
+//
+
+import SwiftUI
+
+@main
+struct StopWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
